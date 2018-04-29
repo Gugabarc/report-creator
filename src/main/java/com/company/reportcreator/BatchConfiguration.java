0c3f4aee5a26bacf4752f5528d08eaaf527b3356
Gustavo <gustavo.pucrs@gmail.com>
2018-04-29 12:44:31 -0300

    Created Step1Configuration class to isolate step1 behavior

--- src/main/java/com/company/reportcreator/BatchConfiguration.java
+++ src/main/java/com/company/reportcreator/BatchConfiguration.java
@@ -6,18 +6,9 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.FlatFileItemWriter;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
-import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
 @EnableBatchProcessing
@@ -33,37 +24,9 @@ public class BatchConfiguration {
     public StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public FlatFileItemReader<Person> reader() {
-        return new FlatFileItemReaderBuilder<Person>()
-            .name("personItemReader")
-            .resource(new ClassPathResource("sample-data.csv"))
-            .delimited().delimiter(";")
-            .names(new String[]{"id", "cpf", "name", "salary"})
-            .fieldSetMapper(new CustomFieldsetMapper())
-            .build();
-    }
-
-    @Bean
     public PersonItemProcessor processor() {
         return personItemProcessor;
     }
-
-	@Bean
-	public FlatFileItemWriter<Person> writer() {
-		FlatFileItemWriter<Person> writer = new FlatFileItemWriter<Person>();
-		writer.setResource(new FileSystemResource("./data/out/teste.dat"));
-
-		DelimitedLineAggregator<Person> aggregator = new DelimitedLineAggregator<Person>();
-		aggregator.setDelimiter("|");
-
-		BeanWrapperFieldExtractor<Person> extractor = new BeanWrapperFieldExtractor<>();
-		extractor.setNames(new String[] { "name", "cpf", "salary" });
-		aggregator.setFieldExtractor(extractor);
-
-		writer.setLineAggregator(aggregator);
-
-		return writer;
-	}
 
     @Bean
     public Job importUserJob(JobCompletionNotificationListener listener, Step step1) {
@@ -76,12 +39,12 @@ public class BatchConfiguration {
     }
 
     @Bean
-    public Step step1(FlatFileItemWriter<Person> writer) {
+    public Step step1(Step1Configuration step1) {
         return stepBuilderFactory.get("step1")
             .<Person, Person> chunk(10)
-            .reader(reader())
+            .reader(step1.fileReader())
             .processor(processor())
-            .writer(writer)
+            .writer(step1.fileWriter())
             .build();
     }
 }
